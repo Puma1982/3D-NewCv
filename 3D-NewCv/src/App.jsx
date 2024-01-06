2dc4e346cdb48e3e3feeb42f33c4eb40e5d7b6f0
VIP-HACKER <griss_hai2005@hotmail.com>
2024-01-06 21:41:45 +0100

    Navbar

--- 3D-NewCv/src/App.jsx
+++ 3D-NewCv/src/App.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
-
+import { Route, BrowserRouter as Router, Routes} from 'react-router-dom';
+import Navbar from './components/Navbar';
 const App = () => {
   return (
-    <div> <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1></div>
+<main className='bg-slate-300/20'>
+  <Router>
+    <Navbar />
+    
+    <Routes>
+      <Route path='/' element={'Home'} />
+      <Route path='/about' element={'About'} />
+      <Route path='/projects' element={'Projects'} />
+      <Route path='/contact' element={'Contact'} />
+
+    </Routes>
+  </Router>
+</main>
   )
 }
 
