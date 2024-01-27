ffc10be5ead956b1d27537a21a9c566e8254700c
VIP-HACKER <griss_hai2005@hotmail.com>
2024-01-27 19:20:56 +0100

    Pages.
    23:03

--- 3D-NewCv/src/components/Navbar.jsx
+++ 3D-NewCv/src/components/Navbar.jsx
@@ -1,6 +1,23 @@
+import { NavLink } from "react-router-dom"
+
 const Navbar = () => {
   return (
-    <div> Navbar </div>
+    <head className="header">
+      <NavLink to="/" className="w-10 h-10 rounded-lg bg-white items-center justify-center flex font-bold shadow-md">
+        <p className="blue-gradient_text">AH</p>
+      </NavLink>
+     <nav className="flex text-lg gap-7 font-medium">
+<NavLink to="/about"className={({isActive})=> isActive ? 'text-blue-500':'text-black'}>
+About
+</NavLink>
+<NavLink to="/projects"className={({isActive})=> isActive ? 'text-blue-500':'text-black'}>
+Projects
+</NavLink>
+
+     </nav>
+
+    </head>
+ 
   )
 }
 
