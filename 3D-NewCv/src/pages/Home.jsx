f8ac68e11cb2c2740679c394fd70b59647aa5e22
VIP-HACKER <griss_hai2005@hotmail.com>
2024-01-28 10:50:21 +0100

    install react-three-fiber
    29:08

--- 3D-NewCv/src/pages/Home.jsx
+++ 3D-NewCv/src/pages/Home.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
-
-function Home() {
+import { Canvas } from '@react-three/fiber'
+ {/* <div className='absolute top-28 left-0 right-0 z-10 flex items-center justify-center'>
+POPUP
+    </div> */} 
+const Home = () => {
   return (
-    <div>Home</div>
+    <section className='w-full h-screen relative'>
+ <Canvas className='w-full h-screen bg-transparent'>
+
+ </Canvas>
+    </section>
   )
 }
 
 export default Home
+{/**https://docs.pmnd.rs/react-three-fiber/getting-started/introduction */}
