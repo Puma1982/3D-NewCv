e1a0af27eea394057906eb66a29f848e72482f13
VIP-HACKER <griss_hai2005@hotmail.com>
2024-01-28 07:57:05 +0100

    bransh1

--- 3D-NewCv/src/components/Navbar.jsx
+++ 3D-NewCv/src/components/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <head className="header">
       <NavLink to="/" className="w-10 h-10 rounded-lg bg-white items-center justify-center flex font-bold shadow-md">
-        <p className="blue-gradient_text">AH</p>
+        <p className="blue-gradient_text">H.G</p>
       </NavLink>
      <nav className="flex text-lg gap-7 font-medium">
 <NavLink to="/about"className={({isActive})=> isActive ? 'text-blue-500':'text-black'}>
