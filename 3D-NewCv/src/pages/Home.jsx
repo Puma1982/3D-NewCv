12b034205ec4b20647016393153358769e2535fb
VIP-HACKER <griss_hai2005@hotmail.com>
2024-02-01 16:09:46 +0100

    loader

--- 3D-NewCv/src/pages/Home.jsx
+++ 3D-NewCv/src/pages/Home.jsx
@@ -1,17 +1,22 @@
-import React from 'react'
-import { Canvas } from '@react-three/fiber'
+import { Canvas } from "@react-three/fiber";
+import { Suspense, useEffect, useRef, useState } from "react";
  {/* <div className='absolute top-28 left-0 right-0 z-10 flex items-center justify-center'>
 POPUP
     </div> */} 
 const Home = () => {
   return (
     <section className='w-full h-screen relative'>
- <Canvas className='w-full h-screen bg-transparent'>
+  <Canvas
+        className={`w-full h-screen bg-transparent `}
+        camera={{ near: 0.1, far: 1000 }}
+      >
 
+ <Suspense fallback={<Loader />}>
+
+</Suspense>
  </Canvas>
     </section>
   )
 }
 
 export default Home
-{/**https://docs.pmnd.rs/react-three-fiber/getting-started/introduction */}
