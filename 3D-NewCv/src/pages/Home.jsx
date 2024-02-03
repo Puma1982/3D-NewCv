4ee9d0f9c8c039f0eabd80e3e5edf9192932372d
VIP-HACKER <griss_hai2005@hotmail.com>
2024-02-03 18:11:50 +0100

    Link for gltf
    https://gltf.pmnd.rs/

--- 3D-NewCv/src/pages/Home.jsx
+++ 3D-NewCv/src/pages/Home.jsx
@@ -7,12 +7,16 @@ const Home = () => {
   return (
     <section className='w-full h-screen relative'>
   <Canvas
-        className={`w-full h-screen bg-transparent `}
+        className={"w-full h-screen bg-transparent"}
         camera={{ near: 0.1, far: 1000 }}
       >
 
  <Suspense fallback={<Loader />}>
-
+<directionalLight />
+<ambientLight />
+<pointLight />
+<spotLight />
+<hemisphereLight />
 </Suspense>
  </Canvas>
     </section>
